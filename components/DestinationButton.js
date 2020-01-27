3a79ea90d971a50105881b2cfd95a599777ef623
Ming Juan <mkjuan@gmail.com>
2020-01-27 11:11:22 -0500

    Feature: Maps, Polyline, Destination Input

--- components/DestinationButton.js
+++ components/DestinationButton.js
@@ -1,60 +1,119 @@
 import React, { Component } from 'react'
-import { StyleSheet, Text, View, TouchableOpacity, Dimensions, TextInput } from 'react-native'
+import { StyleSheet, Text, View, Dimensions, TextInput } from 'react-native'
 
 const WIDTH = Dimensions.get('window').width
+const HEIGHT = Dimensions.get('window').height
 
 export default class DestinationButton extends Component {
 
   render() {
     return (
-      <View style={styles.button}>
-        <TextInput style={styles.destinationText} placeholder={this.props.destination || "Enter Destination"}
-          value={this.props.destination}
-          placeholderTextColor="white"
-          keyboardType="email-address"
-          autoCorrect={false}
-          editable={!this.props.lookingForDriver}
-          onChangeText={destination => {
-            this.props.handleChange("destination", destination)
-            this.props.onChangeDestination(destination)
-          }}
-        />
+      <View style={styles.container}>
+        <View>
+          <View style={styles.inputContainer}>
+            <TextInput style={styles.destinationInput} placeholder={this.props.destination || "Where to...?"}
+              value={this.props.destination}
+              placeholderTextColor="grey"
+              keyboardType="email-address"
+              autoCorrect={false}
+              editable={!this.props.lookingForDriver}
+              onChangeText={destination => {
+                this.props.handleChange("destination", destination)
+                this.props.onChangeDestination(destination)
+              }}
+            />
+          </View>
+          <View style={styles.listContainer}>
+            {this.props.predictions}
+            {/* <View style={styles.buttonContainer}>
+              <TouchableOpacity style={styles.highlight}>
+                <Text style={styles.suggestionText}>Fullstack Academy</Text>
+              </TouchableOpacity>
+            </View>
+            <View style={styles.buttonContainer}>
+              <TouchableOpacity style={styles.highlight}>
+                <Text style={styles.suggestionText}>Central Park</Text>
+              </TouchableOpacity>
+            </View>
+            <View style={styles.buttonContainer}>
+              <TouchableOpacity style={styles.highlight}>
+                <Text style={styles.suggestionText}>Fuller</Text>
+              </TouchableOpacity>
+            </View>
+            <View style={styles.buttonContainer}>
+              <TouchableOpacity style={styles.highlight}>
+                <Text style={styles.suggestionText}>Type</Text>
+              </TouchableOpacity>
+            </View>
+            <View style={styles.buttonContainer}>
+              <TouchableOpacity style={styles.highlight}>
+                <Text style={styles.suggestionText}>Suggest</Text>
+              </TouchableOpacity>
+            </View> */}
+          </View>
+        </View>
       </View>
     )
   }
 }
 
 const styles = StyleSheet.create({
-  // container: {
-  //   zIndex: 9,
-  //   position: 'absolute',
-  //   flexDirection: 'row',
-  //   width: 100,
-  //   height: 60
-  // },
-  destinationText: {
+  buttonContainer: {
+    marginTop: 51,
+  },
+  inputContainer: {
+    zIndex: 9,
+    position: 'absolute',
+  },
+  container: {
+    zIndex: 9,
+    position: 'absolute',
+    flexDirection: 'row',
+    width: (WIDTH)-40,
+    height: 60,
+    backgroundColor: 'black',
+    top: 90,
+    left: 20,
+    borderRadius: 10,
+    alignItems: 'center'
+  },
+  destinationInput: {
+    position: 'absolute',
     fontFamily: Platform.OS === 'android'
       ? 'sans-serif-light'
       : 'Helvetica-Light',
     fontSize: 20,
     color: 'white',
     paddingLeft: 20,
-    width: (WIDTH)-40
+    width: WIDTH-40,
+    top: -10
   },
-  button: {
+  highlight: {
     zIndex: 9,
     position: 'absolute',
     flexDirection: 'row',
     backgroundColor: 'black',
     shadowColor: 'grey',
-    elevation: 5,
-    shadowRadius: 5,
-    shadowOpacity: 2.0,
-    width: (WIDTH)-40,
-    height: 60,
+    width: WIDTH-40,
+    height: 50,
     top: 90,
-    left: 20,
     borderRadius: 10,
-    alignItems: 'center'
+    alignItems: 'center',
+    marginTop: 20
+  },
+  suggestionText: {
+    color: 'white',
+    fontSize: 18,
+    paddingLeft: 20,
+  },
+  listContainer: {
+    position: 'absolute',
+    top: -125
   }
+   // button: {
+  //   shadowColor: 'grey',
+  //   elevation: 5,
+  //   shadowRadius: 5,
+  //   shadowOpacity: 2.0,
+  // },
 })
--- components/Passenger.js
+++ components/Passenger.js
@@ -1,6 +1,7 @@
 import React, { Component }from 'react'
-import MapView, { Marker, PROVIDER_GOOGLE} from 'react-native-maps'
-import { StyleSheet, Text, View, TouchableOpacity, TextInput, Dimensions, TouchableWithoutFeedback, Keyboard } from 'react-native'
+import MapView, { Marker, Polyline, PROVIDER_GOOGLE} from 'react-native-maps'
+import { StyleSheet, Text, View, TouchableOpacity, TouchableHighlight, TextInput, Dimensions, TouchableWithoutFeedback, Keyboard } from 'react-native'
+import PolyLine from "@mapbox/polyline";
 import * as Location from 'expo-location'
 import * as Permissions from 'expo-permissions'
 import DestinationButton from './DestinationButton'
@@ -8,6 +9,9 @@ import { CurrentLocationButton } from './CurrentLocationButton'
 import { CallDriverButton } from './CallDriverButton'
 import { BackToLogin } from './BackToLogin'
 import { expo } from '../app.json'
+
+const apiKey = expo.ios.config.googleMapsApiKey
+const WIDTH = Dimensions.get('window').width
 
 export default class Passenger extends Component {
   constructor() {
@@ -17,34 +21,55 @@ export default class Passenger extends Component {
       hasPermission: false,
       destination: '',
       lookingForDriver: false,
-      predictions: []
+      predictions: [],
+      routeCoords: [],
+      routeResponse: {}
     }
-    this.getLocationAsync = this.getLocationAsync.bind(this)
+    this.getInitialLocation = this.getInitialLocation.bind(this)
     this.centerCurrentLocation = this.centerCurrentLocation.bind(this)
     this.callDriver = this.callDriver.bind(this)
     this.handleChange = this.handleChange.bind(this)
     this.onChangeDestination = this.onChangeDestination.bind(this)
+    this.getRouteDirections = this.getRouteDirections.bind(this)
   }
 
-  componentDidMount() {
-    this.getLocationAsync()
+  async componentDidMount() {
+    this.getInitialLocation()
+    this.watchId = navigator.geolocation.watchPosition(
+      position => {
+        this.setState({
+          region: {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+            latitudeDelta: 0.025,
+            longitudeDelta: 0.025
+          }
+        })
+      },
+      error => console.error(error),
+      { enableHighAccuracy: true, maximumAge: 1200, timeout: 12000 }
+    )
   }
 
-  async getLocationAsync() {
+  componentWillUnmount() {
+    navigator.geolocation.clearWatch(this.watchId);
+  }
+
+  async getInitialLocation() {
     const { status } = await Permissions.askAsync(Permissions.LOCATION)
     if(status !== 'granted') console.log('Permission denied.')
     else (this.setState({
       hasPermission: true
     }))
 
-    const location = await Location.getCurrentPositionAsync({ enableHighAccuracy: true });
+    const location = await Location.getCurrentPositionAsync({ enableHighAccuracy: true })
 
     this.setState({
       region: {
         latitude: location.coords.latitude,
         longitude: location.coords.longitude,
-        latitudeDelta: 0.035,
-        longitudeDelta: 0.035
+        latitudeDelta: 0.025,
+        longitudeDelta: 0.025
       }
     })
   }
@@ -66,24 +91,47 @@ export default class Passenger extends Component {
   }
 
   async onChangeDestination(destination) {
-    const apiKey = expo.ios.config.googleMapsApiKey
-    const apiUrl = `https://maps.googleapis.com/maps/api/place/autocomplete/json?key=${apiKey}
-    &input=${destination}&location=${this.state.region.latitude},${
-      this.state.region.longitude
-    }&radius=2000`;
-
-    try {
-      const result = await fetch(apiUrl);
-      const json = await result.json();
-      this.setState({
-        predictions: json.predictions
-      });
-      console.log(json);
-    } catch (err) {
-      console.error(err);
+    if(destination.length > 3) {
+      const apiUrl = `https://maps.googleapis.com/maps/api/place/autocomplete/json?key=${apiKey}&input=${destination}&location=${this.state.region.latitude},${this.state.region.longitude}&radius=2000`
+      try {
+        const result = await fetch(apiUrl)
+        const json = await result.json()
+        this.setState({
+          predictions: json.predictions
+        })
+        // console.log(json)
+      } catch (err) {
+        console.error(err)
+      }
     }
+  }
 
-    console.log(this.state.predictions)
+  async getRouteDirections(destinationId, destinationName) {
+    try {
+      const res = await fetch(
+        `https://maps.googleapis.com/maps/api/directions/json?origin=${
+          this.state.region.latitude
+        },${
+          this.state.region.longitude
+        }&destination=place_id:${destinationId}&key=${apiKey}`
+      )
+      // console.log(res)
+      const json = await res.json()
+      // console.log(json)
+      const points = PolyLine.decode(json.routes[0].overview_polyline.points)
+      const routeCoords = points.map(point => {
+        return { latitude: point[0], longitude: point[1] }
+      })
+      console.log(routeCoords)
+      this.setState({
+        routeCoords,
+        routeResponse: json
+      })
+      Keyboard.dismiss()
+      return destinationName
+    } catch (error) {
+      console.error(error)
+    }
   }
 
   handleChange(name, value) {
@@ -107,10 +155,32 @@ export default class Passenger extends Component {
   }
 
   render() {
+    const predictions = this.state.predictions.map(prediction => (
+      <View style={styles.buttonContainer}>
+        <TouchableHighlight style={styles.highlight}
+          onPress={async () => {
+            const destinationName = await this.getRouteDirections(
+              prediction.place_id,
+              prediction.structured_formatting.main_text
+            )
+            this.setState({ predictions: [], destination: destinationName })
+            this.map.fitToCoordinates(this.state.routeCoords, {
+              edgePadding: { top: 100, bottom: 100, left: 100, right: 100 }
+            })
+          }}
+          key={prediction.id}
+        >
+            <Text style={styles.suggestionText}>
+              {prediction.structured_formatting.main_text}
+            </Text>
+        </TouchableHighlight>
+      </View>
+    ))
+
     return(
       <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
         <View style={styles.container}>
-          <DestinationButton destination={this.state.destination} handleChange={this.handleChange} lookingForDriver={this.state.lookingForDriver} onChangeDestination={this.onChangeDestination}/>
+          <DestinationButton destination={this.state.destination} handleChange={this.handleChange} lookingForDriver={this.state.lookingForDriver} onChangeDestination={this.onChangeDestination} predictions={predictions}/>
           <BackToLogin passenger={this.props.passenger} handleGuestPassenger={this.props.handleGuestPassenger}/>
           <CurrentLocationButton centerCurrentLocation={this.centerCurrentLocation}/>
           <MapView style={styles.mapStyle}
@@ -118,12 +188,15 @@ export default class Passenger extends Component {
             showsUserLocation={true}
             rotateEnabled={false}
             initialRegion={this.state.region}
-            ref={(map) => {this.map = map}}>
+            ref={(map) => {this.map = map}} >
+            <Polyline
+            coordinates={this.state.routeCoords}
+            strokeWidth={3}
+            strokeColor="black" />
           </MapView>
           { this.state.destination
             ? <CallDriverButton hide={true} callDriver={this.callDriver} lookingForDriver= {this.state.lookingForDriver} />
             : null }
-
         </View>
       </TouchableWithoutFeedback>
     )
@@ -140,5 +213,26 @@ const styles = StyleSheet.create({
   mapStyle: {
     width: Dimensions.get('window').width,
     height: Dimensions.get('window').height,
+  },
+  highlight: {
+    zIndex: 9,
+    position: 'absolute',
+    flexDirection: 'row',
+    backgroundColor: 'black',
+    shadowColor: 'grey',
+    width: WIDTH-40,
+    height: 50,
+    top: 90,
+    borderRadius: 10,
+    alignItems: 'center',
+    marginTop: 20
+  },
+  suggestionText: {
+    color: 'white',
+    fontSize: 18,
+    paddingLeft: 20,
+  },
+  buttonContainer: {
+    marginTop: 51,
   },
 })
